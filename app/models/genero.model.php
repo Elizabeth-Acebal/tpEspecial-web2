361114a2b2aabff9d03cb3228350c0e003b07bc1
Elizabeth-Acebal <elyacebal26@gmail.com>
2023-10-15 15:13:16 -0300

    se agrega config y modifica tpl

--- app/models/genero.model.php
+++ app/models/genero.model.php
@@ -1,5 +1,5 @@
 <?php
-
+require_once 'config.php';
  class GeneroModel{
 
     private $db;
@@ -14,7 +14,10 @@
     //ABRE LA CONEXION A LA BASE DE DATOS
     //solo se puede llamar el mismo , nadie de afuera se va a conectar por eso el private.
     private function getConection() {
-        return new PDO('mysql:host=localhost;dbname=db_peliculas;charset=utf8', 'root', '');
+        return new PDO("mysql:host=".MYSQL_HOST .
+        ";dbname=".MYSQL_DB.";charset=utf8", 
+        MYSQL_USER, MYSQL_PASS);
+        
     }
 
 
