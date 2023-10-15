5734b84be87bab39e274d60a7d029039cc75b4e7
Elizabeth-Acebal <elyacebal26@gmail.com>
2023-10-15 15:23:28 -0300

    agrego deploy

--- app/models/genero.model.php
+++ app/models/genero.model.php
@@ -17,11 +17,20 @@ require_once 'config.php';
         return new PDO("mysql:host=".MYSQL_HOST .
         ";dbname=".MYSQL_DB.";charset=utf8", 
         MYSQL_USER, MYSQL_PASS);
+        $this->deploy();
         
     }
 
-
-
+    private function deploy() {
+        $query = $this->db->query('SHOW TABLES');
+        $tables = $query->fetchAll();
+        if(count($tables) == 0) {
+            $sql =<<<END
+    
+            END;
+            $this->db->query($sql);
+        }
+    }
 
     public function getGeneros(){
         // 1. abro conexión a la DB
--- app/models/pelicula.model.php
+++ app/models/pelicula.model.php
@@ -18,8 +18,21 @@ class PeliculaModel{
     private function getConection() {
         return new PDO("mysql:host=".MYSQL_HOST.";dbname=".MYSQL_DB.";charset=utf8", 
         MYSQL_USER, MYSQL_PASS);
+        $this->deploy();
         
     }
+
+    private function deploy() {
+        $query = $this->db->query('SHOW TABLES');
+        $tables = $query->fetchAll();
+        if(count($tables) == 0) {
+            $sql =<<<END
+    
+            END;
+            $this->db->query($sql);
+        }
+    }
+
 
         // obtiene la lista de peliculas de la DB.
     function getPeliculas() {
